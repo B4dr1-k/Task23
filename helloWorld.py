9168a0541b4e7f8e5e3659f4f0f188bdcaf77843
B4dr1-k <b4dr1@outlook.com>
2024-03-19 23:40:53 +0000

    Added comments and a function

--- helloWorld.py
+++ helloWorld.py
@@ -3,8 +3,13 @@ Task 23 - Learn GIT
 First fix - Adding comments
 '''
 
+def print_line():
+    print("------------------------------------")
+
 #Getting a string from the user
 message = input("enter a string: ")
 
 #Printing the input back to user
+print_line()
 print(message)
+print_line()
