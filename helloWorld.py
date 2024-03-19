05a6bf1475759055979580edae2cd3e90a6a7135
B4dr1-k <b4dr1@outlook.com>
2024-03-19 23:24:46 +0000

    Added comments

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,10 @@
+'''
+Task 23 - Learn GIT
+First fix - Adding comments
+'''
+
+#Getting a string from the user
 message = input("enter a string: ")
+
+#Printing the input back to user
 print(message)
