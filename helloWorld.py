f54a38915ae133e50d2cb508a3dc75c82ff80e90
B4dr1-k <b4dr1@outlook.com>
2024-03-20 00:06:41 +0000

    Added validation

--- helloWorld.py
+++ helloWorld.py
@@ -1,14 +1,23 @@
 '''
 Task 23 - Learn GIT
-First fix - Adding comments
+First fix - Adding comments and a line function
+Second fix - adding a check to see if it is string
 '''
 
 def print_line():
     print("------------------------------------")
 
-#Getting a string from the user
-message = input("enter a string: ")
-
+#Getting a string from the user and ensuring it is not empty
+input = ''
+while (input == ''):
+    message = input("enter a string: ")
+    #remove any empty strings
+    input = input.strip()
+    if input != '': 
+        break
+    elif input == '':
+        continue
+    
 #Printing the input back to user
 print_line()
 print(message)
